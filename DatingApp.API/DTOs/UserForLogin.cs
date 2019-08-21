d2d1093713ed2374c7d354200747cf8cf43fb2d6
Colburn Sanders <colburn.sanders@eagletg.com>
2019-08-21 11:24:31 -0500

    creating login method

---
+++ DatingApp.API/DTOs/UserForLogin.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.DTOs
+{
+    public class UserForLogin
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
