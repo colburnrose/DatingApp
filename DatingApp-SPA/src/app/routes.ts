babbc4023595bf9485e64d3519bfa5d54bccc31f
Colburn Sanders <colburn.sanders@eagletg.com>
2019-09-15 12:32:17 -0500

    protecting routes with single route guard using dummy routes

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -3,12 +3,20 @@ import { MembersComponent } from './members/members.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
   // each route is an object
-  { path: 'home', component: HomeComponent},
-  { path: 'members', component: MembersComponent},
-  { path: 'lists', component: ListsComponent},
-  { path: 'messages', component: MessagesComponent},
-  { path: '**', redirectTo: 'home', pathMatch: 'full'}, // matches the full path of the url to redirect
+  { path: '', component: HomeComponent},
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MembersComponent},
+      { path: 'lists', component: ListsComponent},
+      { path: 'messages', component: MessagesComponent},
+    ]
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full'}, // matches the full path of the url to redirect
 ];
